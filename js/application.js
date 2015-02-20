5aca256650fd7cd2e9aadc6be374948ee2ea8e25
Roman Stranghöner <roman.stranghoener@innoq.com>
2015-02-20 11:33:33 +0100

    add fallback link for Disqus

--- js/application.js
+++ js/application.js
@@ -1,27 +1,29 @@
 (function($){
-	
+
 	$(function() {
-	  
-		$('#disqus_thread').disqus({
-			domain:     "roca-style",
-			title:      document.title,
-			developer:  window.location.hostname.match(/localhost|0\.0\.0\.0|127\.0\.0\.1/) ? 1 : 0,
-			show_count: true,
-			prettify:   false,
-			markdown:   false,
-			// iframe_css: "http://somewhere.com/stylesheets/disqus.css",
-			ready: function() {
-				// this is when your disqus comments finally load
-				console.log("Comment count: " + $.disqus.commentCount().toString());
-				console.log("Reaction count: " + $.disqus.reactionCount().toString());
-			},
-			added: function(comments) {
-				// do something with the newly added comment divs.
-			},
-			edit: function(textarea) {
-				// called when someone clicks the "reply" button.
-			}
-		});
+	  if($.disqus) {
+			$('#disqus_thread').disqus({
+				domain:     "roca-style",
+				title:      document.title,
+				developer:  window.location.hostname.match(/localhost|0\.0\.0\.0|127\.0\.0\.1/) ? 1 : 0,
+				show_count: true,
+				prettify:   false,
+				markdown:   false,
+				// iframe_css: "http://somewhere.com/stylesheets/disqus.css",
+				ready: function() {
+					// this is when your disqus comments finally load
+					console.log("Comment count: " + $.disqus.commentCount().toString());
+					console.log("Reaction count: " + $.disqus.reactionCount().toString());
+					$('#disqus_fallback').remove();
+				},
+				added: function(comments) {
+					// do something with the newly added comment divs.
+				},
+				edit: function(textarea) {
+					// called when someone clicks the "reply" button.
+				}
+			});
+		}
 	});
 
-}(jQuery))
+}(jQuery));
